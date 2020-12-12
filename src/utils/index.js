1340b60e1dbbd0a0a4de10bccb074f65d202ec5e
xie_sheng_jin <shengjin.xie@1-1dr.com>
2020-12-12 16:02:50 +0800

    index

--- src/utils/index.js
+++ src/utils/index.js
@@ -53,6 +53,13 @@ const UTILS = {
       }
     }
     return objClone;
+  },
+  guid(){// 生成唯一的UUID
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,function(c){
+      var r = Math.random()*16 | 0,
+      v = c=='x'? r:(r & 0x3 | 0x8);
+      return v.toString(16);
+    })
   }
 }
 export default UTILS;
